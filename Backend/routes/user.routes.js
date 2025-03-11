bab5d8b8158f9f02650829f8ec0b509af529a2e8
DEV0110010 <newera.deepanshu@gmail.com>
2025-03-11 13:24:42 +0530

    commit

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router();
 const {body} = require("express-validator");
 const userController = require('../controllers/user.controller')
+const authMiddleware = require('../middlewares/auth.middleware')
 
 router.post('/register', [body('email').isEmail().withMessage('Invalid Email'),
     body('fullname.firstname').isLength({min: 3}).withMessage('First name must be atleat 3 characters long'),
@@ -14,6 +15,7 @@ router.post('/login', [body('email').isEmail().withMessage('Invalid Email'),
     userController.loginUser
 )
 
-
+router.get('/profile',authMiddleware.authUser, userController.getUserProfile);
+router.get('/logout', authMiddleware.authUser, userController.logoutUser);
 
 module.exports = router;
